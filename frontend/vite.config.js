84cf1ee0dacc817166fa8c416b898c0fbd699daa
Matt Rush <matthew.rush@cosmient.com>
2026-04-19 00:41:21 +0100

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,10 +11,7 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true
       }
-    },
-    allowedHosts: [
-      'multitask-snowbound-crepe.ngrok-free.dev'
-    ]
+    }
   },
   test: {
     globals: true,
